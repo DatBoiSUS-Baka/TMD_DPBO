70784cd7408a87300ff210bb8eadf9589b3bedcd
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-06-06 19:01:14 +0700

    Menambahkan komen pada App.java

--- TMD/src/App.java
+++ TMD/src/App.java
@@ -1,7 +1,19 @@
+/*
+ * ===================================================
+ * Filename     : DB.java
+ * Programmer   : Kasyful Haq Bachariputra
+ * Date         : 6 June 2025
+ * Email        : kasyfulhaqb@upi.edu
+ * Deskripsi    : File App untuk dapat bisa menjalankan game
+ * 
+ * ===================================================
+ */
+
 import view.TampilUser;
 
 public class App {
     public static void main(String[] args){
+        // Memanggil modul view untuk menampilkan user
         TampilUser tampiluser = new TampilUser();
         tampiluser.tampil();
     }
