f11385592f6cbf5ef72496f17de03bc718ec4071
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-06-10 20:05:17 +0700

    Menyesuaikan App.java untuk menjalankan game

--- TMD/src/App.java
+++ TMD/src/App.java
@@ -9,19 +9,67 @@
  * ===================================================
  */
 
+import java.awt.CardLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
-import view.TampilTHasil;
+
+import model.Pemain;
+import presenter.GamePresenter;
+import view.MainGameView;
+import view.MenuView;
 
 public class App {
     public static void main(String[] args){
-        // Memanggil modul view untuk menampilkan user
         SwingUtilities.invokeLater(new Runnable() {
             // Menggunakan invokeLater agar tidak terjadi error ketika menjalankan App
             public void run(){
-                TampilTHasil tampiluser = new TampilTHasil();
-                tampiluser.setVisible(true);
-
+                mulaiGame();
             }
         });
     }
+
+    private static void mulaiGame(){
+        // Buat frame terlebih dahulu
+        JFrame frame = new JFrame("TMD Bola-bola");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Menggunakan CardLayout untuk merubah halaman
+        CardLayout cardLayout = new CardLayout();
+        JPanel mainPanel = new JPanel(cardLayout);
+
+        // Instansiasi berbagai macam view yang ada
+        MenuView menuView = new MenuView();
+        MainGameView gameView = new MainGameView();
+        
+        // Tambahkan view ke Panel
+        mainPanel.add(menuView, "MENU");
+        mainPanel.add(gameView, "GAME");
+
+        // Membuat model dan presenter untuk bagian Game utama (Gameloop)
+        Pemain pemain = new Pemain(50, 50);
+        GamePresenter presenter = new GamePresenter(pemain, gameView);
+        gameView.setPemain(pemain);
+
+        // Logika untuk mengubah halaman
+        menuView.addStartButtonListener(e -> {
+            cardLayout.show(mainPanel, "GAME"); 
+            gameView.setFocusable(true);
+        	gameView.requestFocusInWindow();
+            presenter.startGame();
+        });
+
+        // Setup dari frame
+        frame.add(mainPanel);
+        frame.pack();
+        frame.setSize(800, 700);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        // Tunjukkan menu terlebih dahulu
+        cardLayout.show(mainPanel, "MENU");
+
+    }
+
 }
