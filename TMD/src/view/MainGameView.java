3dd9b537ca199c49c9fc737523ea3e43ad25831f
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-06-10 19:57:31 +0700

    Menambagkan MainGameView.java sebagai view utama yang dipanggil ketika menjalankan game

---
+++ TMD/src/view/MainGameView.java
@@ -0,0 +1,40 @@
+package view;
+
+import java.awt.Dimension;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+import model.Pemain;
+import view.GameLoopContract;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class MainGameView extends JPanel implements GameLoopContract{
+    private Pemain pemain;
+
+    public MainGameView(){
+        setPreferredSize(new Dimension(800, 600));
+    }
+
+    public void setPemain(Pemain pemain) { this.pemain = pemain; }
+    
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        if (pemain != null) {
+            g.setColor(Color.GREEN);    
+            g.fillRect(pemain.getPosX(), pemain.getPosY(), pemain.getSize(), pemain.getSize());
+        }
+    }
+
+    @Override
+    public void refreshView(){
+        repaint();
+    }
+
+    @Override
+    public void addKeyListener(KeyListener key){
+        super.addKeyListener(key);
+    }
+}
