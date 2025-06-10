f5603a64cd5b3c7babf74fefba0316138e187aea
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-06-10 20:40:47 +0700

    Hapus salah satu library

--- TMD/src/presenter/GamePresenter.java
+++ TMD/src/presenter/GamePresenter.java
@@ -4,8 +4,6 @@ import model.Pemain;
 import view.MainGameView;
 
 import javax.swing.Timer;
-import javax.swing.plaf.basic.BasicComboBoxUI.KeyHandler;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
