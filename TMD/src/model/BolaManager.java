9473e16199788d3bbbfcd67184e6a34733590cdf
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-06-11 20:02:50 +0700

    Menambah method pada BolaManager.java untuk spawn bola dan update bola

--- TMD/src/model/BolaManager.java
+++ TMD/src/model/BolaManager.java
@@ -12,22 +12,57 @@ package model;
  */
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.random.RandomGenerator;
 
 import model.Bola;
 
 public class BolaManager {
     private ArrayList<Bola> collectionBola = new ArrayList<Bola>();
+    private RandomGenerator random = RandomGenerator.getDefault();
 
     public BolaManager(){
 
     }
 
-    public void spawnBola(int jumlahBola, int screenWidth){
+    public void spawnBola(int jumlahBola, int screenWidth, int screenHeight){
         collectionBola.clear();
-
+        
         for(int i = 0; i < jumlahBola; i++){
-            collectionBola.add(new Bola(screenWidth/3, 3));
+            spawnSatuBola(screenWidth, screenHeight);
+        }
+    }
 
+    public void spawnSatuBola(int screenWidth, int screenHeight){
+        boolean spawnLeft = random.nextBoolean();
+        int direction;
+        int startX;
+        int startY;
+        
+        if (spawnLeft) {
+            startX = -50;
+            startY = screenHeight / 8;
+            direction = 1;
+        }else{
+            startX = screenWidth + 50;
+            startY = screenHeight * 7 / 8;
+            direction = -1;
+        }
+
+        collectionBola.addLast(new Bola(startX, startY));
+        collectionBola.getLast().setDirection(direction);
+    }
+
+    public void updateAllBolas(int screenWidth){
+        Iterator<Bola> it = collectionBola.iterator();
+        while (it.hasNext()) {
+            Bola currentBola = it.next();
+            currentBola.gerak();
+            if (currentBola.getPosX() > screenWidth && currentBola.getDirection() == 1) {
+                it.remove();
+            }else if(currentBola.getPosX() < (-1 * currentBola.getPosX()) && currentBola.getDirection() == -1){
+                it.remove();
+            }
         }
     }
 
