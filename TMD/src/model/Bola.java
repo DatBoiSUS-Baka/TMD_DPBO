d8dd3fe96b34cddd327804e03fb2da79afd61614
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-06-11 18:07:03 +0700

    Menambah method gerak dan atribut size pada Bola.java

--- TMD/src/model/Bola.java
+++ TMD/src/model/Bola.java
@@ -1,20 +1,35 @@
 package model;
 
+/*
+ * 11 Juni 2025
+ */
+
 public class Bola {
     private int positionX;
     private int positionY;
     private int speed;
     private int value;
+    private int size;
 
     public Bola(int startX, int startY){
         this.positionX = startX;
         this.positionY = startY;
         this.speed = 5;
         this.value = 0;
+        this.size = 50;
     }
 
     public int getPosX() { return this.positionX; }
     public int getPosY() { return this.positionY; }
     public int getSpeed() { return this.speed; }
     public int getValue() { return this.value; }
+    public int getSize() { return this.size; }
+
+    public void gerak(int dx, int dy){
+        /*
+         * Method untuk membuat bola dapat bergerak
+         */
+        int newX = this.positionX + (dx * speed);
+        int newY = this.positionY + (dy * speed);
+    }
 }
